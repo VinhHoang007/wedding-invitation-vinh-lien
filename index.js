c21d27e82542e42a08e9da092cbeeaf47200c993
Vinh Hoang Ngoc <vinhhoangngoc22@gmail.com>
2023-05-07 00:52:21 +0700

    test get params

--- index.js
+++ index.js
@@ -5,10 +5,11 @@ $(document).ready(function () {
     function windownStart() {
         setTimeout(() => {
             window.scrollTo(0, 0);
-            // console.log(document.getElementById("my-audio").muted);
-            // document.getElementById("audio").muted = true;
+            var url_string = window.location.href;
+            var url = new URL(url_string);
+            var name = url.searchParams.get("name");
+            document.getElementById("name").innerText = name;
         }, 100);
     }
     windownStart();
-    document.querySelector("video").muted = false;
 });
